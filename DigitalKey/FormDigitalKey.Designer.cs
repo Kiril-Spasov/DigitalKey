9fb727b7b3bf18721b14f155989c2fda31013755
Kiril Spasov <spasov.kiril@gmail.com>
2022-12-02 15:50:01 +0200

    Add project files.

---
+++ DigitalKey/FormDigitalKey.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DigitalKey
+{
+    partial class FormDigitalKey
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnReadDigitalKey = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnReadDigitalKey
+            // 
+            this.BtnReadDigitalKey.Location = new System.Drawing.Point(44, 61);
+            this.BtnReadDigitalKey.Name = "BtnReadDigitalKey";
+            this.BtnReadDigitalKey.Size = new System.Drawing.Size(171, 54);
+            this.BtnReadDigitalKey.TabIndex = 0;
+            this.BtnReadDigitalKey.Text = "Read Digital Key";
+            this.BtnReadDigitalKey.UseVisualStyleBackColor = true;
+            this.BtnReadDigitalKey.Click += new System.EventHandler(this.BtnReadDigitalKey_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(244, 61);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(484, 344);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormDigitalKey
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnReadDigitalKey);
+            this.Name = "FormDigitalKey";
+            this.Text = "Digital Key";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnReadDigitalKey;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ DigitalKey/FormDigitalKey.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace DigitalKey
+{
+    public partial class FormDigitalKey : Form
+    {
+        public FormDigitalKey()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnReadDigitalKey_Click(object sender, EventArgs e)
+        {
+            string line = "";
+            int lineCount = 0;
+
+            string path = Application.StartupPath + @"\digitalkey.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            bool finished = false;
+
+            while (!finished)
+            {
+                line = streamReader.ReadLine();
+
+                if (line == null)
+                {
+                    finished = true;
+                }
+                else
+                {
+                    if (line != "")
+                    {
+                        lineCount++;
+
+                        TxtResult.Text += ReadDigitalKey(line);
+
+                        if (lineCount == 7)
+                        {
+                            TxtResult.Text += Environment.NewLine;
+                            lineCount = 0;
+                        }
+                    }  
+                }
+            }
+        }
+
+        private string ReadDigitalKey(string keyTheetHight)
+        {
+            string character;
+            string result = "";
+            int hCount = 0;
+
+            for (int i = 0; i <= 4; i++)
+            {
+                character = keyTheetHight.Substring(i, 1);
+
+                if (i == 0 && character == ".")
+                {
+                    result += "0";
+                }
+                else if (character == "#")
+                {
+                    hCount++;
+                }
+            }
+
+            return result = hCount.ToString();
+        }
+    }
+}
